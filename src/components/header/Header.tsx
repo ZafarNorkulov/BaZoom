20f8d1cd59b2271aeca6f7e616c740413fc2676c
ZafarNorkulov <norqulovzafar123@gmail.com>
2025-02-01 12:53:46 +0500

    change

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -22,13 +22,12 @@ function Header() {
 
   const navigate = useNavigate();
   const userId = 1742336847;
-  const daats = `user=%7B%22id%22%3A1742336847%2C%22first_name%22%3A%22Zafar%22%2C%22last_name%22%3A%22Norkulov%22%2C%22username%22%3A%22Zafar_Norkulov%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F4OQHgqkBTQzZX8WGfD-hlRPvpUjXNuMNwFMpYDjE2pQ.svg%22%7D&chat_instance=-5929298958289053064&chat_type=sender&auth_date=1738395817&signature=o07nFOnIWPHbKHyUvf8HDVO-LQdRtSFWdlWKBBxImthTc5yaCg1rD9F1YQoGgNZU_a8ziG3x0GYJFsoxgaqQCA&hash=b785da2d494b89d2e7e1023bcd6ea4b2815b2de1009acf2555d142dae3d08aa4`
 
-  async function getData (data:string): Promise<any>{
+  async function getData (){
     fetch(`https://bot.bazoom.ru/api/users/profile`, {
       method: "GET",
       headers: {
-        "Init-Data": data,
+        "Init-Data": `user=%7B%22id%22%3A1742336847%2C%22first_name%22%3A%22Zafar%22%2C%22last_name%22%3A%22Norkulov%22%2C%22username%22%3A%22Zafar_Norkulov%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F4OQHgqkBTQzZX8WGfD-hlRPvpUjXNuMNwFMpYDjE2pQ.svg%22%7D&chat_instance=-5929298958289053064&chat_type=sender&auth_date=1738395817&signature=o07nFOnIWPHbKHyUvf8HDVO-LQdRtSFWdlWKBBxImthTc5yaCg1rD9F1YQoGgNZU_a8ziG3x0GYJFsoxgaqQCA&hash=b785da2d494b89d2e7e1023bcd6ea4b2815b2de1009acf2555d142dae3d08aa4`,
       },
     })
       .then((res) => res.json())
@@ -37,7 +36,7 @@ function Header() {
 
   useEffect(() => {
     if (!(userId)) return;
-    getData(daats);
+    getData();
   }, [userId]);
 
   console.log(gameData);
